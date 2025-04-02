fe21cf81acfb11efe24c3ee89244f0a280155c93
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-03 01:48:09 +0300

    contacts content

---
+++ src/app/contacts/contacts-content/contacts-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts-content',
+  imports: [],
+  templateUrl: './contacts-content.component.html',
+  styleUrl: './contacts-content.component.css'
+})
+export class ContactsContentComponent {
+
+}
