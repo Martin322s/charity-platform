bb14de97d4d9e70e3f3d332e169af89f9c42ec6a
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-03 01:47:21 +0300

    setup routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { AboutPageComponent } from './about/about-page/about-page.component';
 import { ServicesPageComponent } from './services/services-page/services-page.component';
 import { CausesPageComponent } from './causes/causes-page/causes-page.component';
 import { CauseDetailsPageComponent } from './cause-details/cause-details-page/cause-details-page.component';
+import { ContactsPageComponent } from './contacts/contacts-page/contacts-page.component';
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'about', component: AboutPageComponent },
 	{ path: 'services', component: ServicesPageComponent },
 	{ path: 'causes', component: CausesPageComponent },
-	{ path: 'cause-details/:causeId', component: CauseDetailsPageComponent }
+	{ path: 'cause-details/:causeId', component: CauseDetailsPageComponent },
+	{ path: 'contacts', component: ContactsPageComponent }
 ];
