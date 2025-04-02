e2b4e5655f33efef972cc55f65bef2a2a9cb38b2
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-03 01:47:55 +0300

    contacts header

---
+++ src/app/contacts/contacts-header/contacts-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts-header',
+  imports: [],
+  templateUrl: './contacts-header.component.html',
+  styleUrl: './contacts-header.component.css'
+})
+export class ContactsHeaderComponent {
+
+}
