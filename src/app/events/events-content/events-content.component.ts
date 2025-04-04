6e5bd11635f58ca8c239783e53e42597048255a3
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-05 01:59:36 +0300

    events content

---
+++ src/app/events/events-content/events-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events-content',
+  imports: [],
+  templateUrl: './events-content.component.html',
+  styleUrl: './events-content.component.css'
+})
+export class EventsContentComponent {
+
+}
