296feec82116b3f4c38d90404f33e62c1f30a389
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-05 01:59:23 +0300

    events header

---
+++ src/app/events/events-header/events-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events-header',
+  imports: [],
+  templateUrl: './events-header.component.html',
+  styleUrl: './events-header.component.css'
+})
+export class EventsHeaderComponent {
+
+}
