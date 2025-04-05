5c28d9f5c20b87771de9dfcdc46e417eb088d6c0
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-06 01:24:46 +0300

    cause details hero

---
+++ src/app/cause-details/casue-details-hero/casue-details-hero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-casue-details-hero',
+  imports: [],
+  templateUrl: './casue-details-hero.component.html',
+  styleUrl: './casue-details-hero.component.css'
+})
+export class CasueDetailsHeroComponent {
+
+}
