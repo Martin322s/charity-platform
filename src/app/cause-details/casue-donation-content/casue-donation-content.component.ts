aa201d3a864ee2a22debaa23bba399c490154be5
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-06 01:25:05 +0300

    cause details content

---
+++ src/app/cause-details/casue-donation-content/casue-donation-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-casue-donation-content',
+  imports: [],
+  templateUrl: './casue-donation-content.component.html',
+  styleUrl: './casue-donation-content.component.css'
+})
+export class CasueDonationContentComponent {
+
+}
