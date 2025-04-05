76b96452930a0ebf809548d1c05c8a4f1d93823d
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-06 01:25:55 +0300

    cause details content

--- src/app/causes/causes-content/causes-content.component.ts
+++ src/app/causes/causes-content/causes-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-causes-content',
-  imports: [],
-  templateUrl: './causes-content.component.html',
-  styleUrl: './causes-content.component.css'
+	selector: 'app-causes-content',
+	imports: [RouterModule],
+	templateUrl: './causes-content.component.html',
+	styleUrl: './causes-content.component.css'
 })
 export class CausesContentComponent {
 
