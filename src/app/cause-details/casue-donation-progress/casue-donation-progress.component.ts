3b5261fefacb43441b600ec798cbc60e17e07bf6
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-06 01:25:22 +0300

    cause details progress

---
+++ src/app/cause-details/casue-donation-progress/casue-donation-progress.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-casue-donation-progress',
+  imports: [],
+  templateUrl: './casue-donation-progress.component.html',
+  styleUrl: './casue-donation-progress.component.css'
+})
+export class CasueDonationProgressComponent {
+
+}
