138c2fe327e9013507f668f5c58daa557ef781eb
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-06 01:23:54 +0300

    add resources and setup routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home-page/home/home.component';
 import { AboutPageComponent } from './about/about-page/about-page.component';
 import { ServicesPageComponent } from './services/services-page/services-page.component';
 import { CausesPageComponent } from './causes/causes-page/causes-page.component';
+import { CauseDetailsPageComponent } from './cause-details/cause-details-page/cause-details-page.component';
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'about', component: AboutPageComponent },
 	{ path: 'services', component: ServicesPageComponent },
-	{ path: 'causes', component: CausesPageComponent }
+	{ path: 'causes', component: CausesPageComponent },
+	{ path: 'cause-details/:causeId', component: CauseDetailsPageComponent }
 ];
