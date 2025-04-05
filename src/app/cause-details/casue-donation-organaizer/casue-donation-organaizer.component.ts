19417ce440efd41a47bba7457c19b0ede642728c
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-06 01:25:44 +0300

    cause details organaizer

---
+++ src/app/cause-details/casue-donation-organaizer/casue-donation-organaizer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-casue-donation-organaizer',
+  imports: [],
+  templateUrl: './casue-donation-organaizer.component.html',
+  styleUrl: './casue-donation-organaizer.component.css'
+})
+export class CasueDonationOrganaizerComponent {
+
+}
