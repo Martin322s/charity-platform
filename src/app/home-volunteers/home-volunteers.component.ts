f889ce41865fee9763daf5d94ab58b1266f6b115
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-09 18:51:29 +0300

    home volunteers component

---
+++ src/app/home-volunteers/home-volunteers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-volunteers',
+  imports: [],
+  templateUrl: './home-volunteers.component.html',
+  styleUrl: './home-volunteers.component.css'
+})
+export class HomeVolunteersComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HomeCarouselComponent } from "../home-carousel/home-carousel.component";
 import { HomeAboutSectionComponent } from "../home-about-section/home-about-section.component";
 import { HomeServicesComponent } from '../home-services/home-services.component';
+import { HomeVolunteersComponent } from "../home-volunteers/home-volunteers.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HomeCarouselComponent, HomeAboutSectionComponent, HomeServicesComponent],
+  imports: [HomeCarouselComponent, HomeAboutSectionComponent, HomeServicesComponent, HomeVolunteersComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
