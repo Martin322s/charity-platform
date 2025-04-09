fcca5bcffd318eceeb8bd7e0767e1e6edf9ed2a0
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-10 00:25:26 +0300

    setup routing and add about page

---
+++ src/app/about/about-page/about-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AboutHeaderComponent } from "../about-header/about-header.component";
+
+@Component({
+  selector: 'app-about-page',
+  imports: [AboutHeaderComponent],
+  templateUrl: './about-page.component.html',
+  styleUrl: './about-page.component.css'
+})
+export class AboutPageComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home-page/home/home.component';
+import { AboutPageComponent } from './about/about-page/about-page.component';
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent },
+	{ path: 'about', component: AboutPageComponent },
 ];
