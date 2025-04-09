3ffda11a9035500b1d8671266402f9d33c2d7596
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-10 00:56:54 +0300

    causes content component

---
+++ src/app/causes/causes-content/causes-content.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-causes-content',
+  imports: [],
+  templateUrl: './causes-content.component.html',
+  styleUrl: './causes-content.component.css'
+})
+export class CausesContentComponent {
+
+}
