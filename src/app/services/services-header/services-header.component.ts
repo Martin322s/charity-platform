57d439d1160b5800dbcce993ecf76e0e282ddbe0
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-10 00:52:07 +0300

    services header component

---
+++ src/app/services/services-header/services-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services-header',
+  imports: [],
+  templateUrl: './services-header.component.html',
+  styleUrl: './services-header.component.css'
+})
+export class ServicesHeaderComponent {
+
+}
