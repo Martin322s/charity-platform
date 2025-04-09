f11f3f23113cd136e7daa9c63903b73c79cf701e
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-10 00:51:48 +0300

    add service examples component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home-page/home/home.component';
 import { AboutPageComponent } from './about/about-page/about-page.component';
+import { ServicesPageComponent } from './services/services-page/services-page.component';
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent },
 	{ path: 'about', component: AboutPageComponent },
+	{ path: 'services', component: ServicesPageComponent }
 ];
---
+++ src/app/services/service-examples/service-examples.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-service-examples',
+  imports: [],
+  templateUrl: './service-examples.component.html',
+  styleUrl: './service-examples.component.css'
+})
+export class ServiceExamplesComponent {
+
+}
