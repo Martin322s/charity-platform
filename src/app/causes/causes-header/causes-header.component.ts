49635524e8526fe40f9f3e6fd29629dd5f5315f6
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-10 00:56:37 +0300

    causes header component

---
+++ src/app/causes/causes-header/causes-header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-causes-header',
+  imports: [],
+  templateUrl: './causes-header.component.html',
+  styleUrl: './causes-header.component.css'
+})
+export class CausesHeaderComponent {
+
+}
