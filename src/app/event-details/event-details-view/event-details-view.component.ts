103d1b8a75a6b7cd84bcafa7009c95faf770b235
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-11 13:09:04 +0300

    event details view

---
+++ src/app/event-details/event-details-view/event-details-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-details-view',
+  imports: [],
+  templateUrl: './event-details-view.component.html',
+  styleUrl: './event-details-view.component.css'
+})
+export class EventDetailsViewComponent {
+
+}
