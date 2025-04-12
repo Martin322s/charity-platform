95a223bc06c452567530fd32d0488c3e5434b14f
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-12 16:10:39 +0300

    profile-causes component

---
+++ src/app/profile/profile-causes/profile-causes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-causes',
+  imports: [],
+  templateUrl: './profile-causes.component.html',
+  styleUrl: './profile-causes.component.css'
+})
+export class ProfileCausesComponent {
+
+}
