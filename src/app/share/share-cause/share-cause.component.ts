52b8e65c96fdac3f78a5d5990d7d4b5376d13eef
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-12 16:09:09 +0300

    share-cause component

---
+++ src/app/share/share-cause/share-cause.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-share-cause',
+  imports: [],
+  templateUrl: './share-cause.component.html',
+  styleUrl: './share-cause.component.css'
+})
+export class ShareCauseComponent {
+
+}
