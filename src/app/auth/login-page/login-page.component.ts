6f1f5f82963a7ab5a555b558a5edff4184605422
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-14 21:37:01 +0300

    login and register translate

--- src/app/auth/login-page/login-page.component.ts
+++ src/app/auth/login-page/login-page.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from '../login-form/login-form.component';
 import { RegisterFormComponent } from '../register-form/register-form.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-login-page',
-  imports: [LoginFormComponent, RegisterFormComponent],
-  templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+	selector: 'app-login-page',
+	standalone: true,
+	imports: [CommonModule, LoginFormComponent, RegisterFormComponent],
+	templateUrl: './login-page.component.html',
+	styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+	activeForm: 'login' | 'register' = 'login';
 
+	setForm(form: 'login' | 'register') {
+		this.activeForm = form;
+	}
 }
