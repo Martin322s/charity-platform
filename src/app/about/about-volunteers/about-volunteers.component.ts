1867c564250663916e1f7fdbbcd5fe84d91b93e9
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-14 21:37:25 +0300

    new about volunteers section

---
+++ src/app/about/about-volunteers/about-volunteers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-volunteers',
+  imports: [],
+  templateUrl: './about-volunteers.component.html',
+  styleUrl: './about-volunteers.component.css'
+})
+export class AboutVolunteersComponent {
+
+}
