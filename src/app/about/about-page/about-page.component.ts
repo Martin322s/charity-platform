8c2d821978e9a4de9cf9b15460f9b899f22e09fd
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-14 21:38:15 +0300

    about section new changes

--- src/app/about/about-page/about-page.component.ts
+++ src/app/about/about-page/about-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { AboutHeaderComponent } from "../about-header/about-header.component";
-import { HomeAboutSectionComponent } from "../../home-page/home-about-section/home-about-section.component";
 import { HomeVolunteersComponent } from "../../home-page/home-volunteers/home-volunteers.component";
+import { AboutSectionComponent } from "../about-section/about-section.component";
+import { AboutVolunteersComponent } from "../about-volunteers/about-volunteers.component";
 
 @Component({
   selector: 'app-about-page',
-  imports: [AboutHeaderComponent, HomeAboutSectionComponent, HomeVolunteersComponent],
+  imports: [AboutHeaderComponent, AboutSectionComponent, AboutVolunteersComponent],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.css'
 })
