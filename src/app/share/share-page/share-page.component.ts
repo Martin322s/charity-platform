99423d974b64a952a98a01dc5672c0e2eb02ed80
Martin Sofroniev <m.sofroniev12@gmail.com>
2025-04-14 21:35:50 +0300

    share page dynamic tab changes

--- src/app/share/share-page/share-page.component.ts
+++ src/app/share/share-page/share-page.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { ShareEventComponent } from "../share-event/share-event.component";
 import { ShareCauseComponent } from "../share-cause/share-cause.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-share-page',
-  imports: [ShareEventComponent, ShareCauseComponent],
-  templateUrl: './share-page.component.html',
-  styleUrl: './share-page.component.css'
+	selector: 'app-share-page',
+	standalone: true,
+	imports: [ShareEventComponent, ShareCauseComponent, CommonModule],
+	templateUrl: './share-page.component.html',
+	styleUrls: ['./share-page.component.css']
 })
 export class SharePageComponent {
+	activeTab: 'event' | 'donation' = 'event';
 
+	setTab(tab: 'event' | 'donation') {
+		this.activeTab = tab;
+	}
 }
